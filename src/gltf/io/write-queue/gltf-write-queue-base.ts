98e6c7c84b6b04bd823c8dfec3574f330d1b3d56
onsummer <onsummer@foxmail.com>
2021-05-10 18:43:10 +0800

    [update] husky -> git pre commit hook; [add] writequeue docs.

--- src/gltf/io/write-queue/gltf-write-queue-base.ts
+++ src/gltf/io/write-queue/gltf-write-queue-base.ts
@@ -1,12 +1,12 @@
+import { IGLTFWriteAction } from "src"
 import { GLTFDocument } from "src/gltf/core"
-import IGLTFWriteAction from "../write-action"
 
 class GLTFWriteQueueBase {
   actions: IGLTFWriteAction[] = []
   doc: GLTFDocument
 
   execute() {
-    this.actions.forEach(action => {
+    this.actions.forEach((action) => {
       action.submit(this.doc)
       console.log(action.type)
     })
