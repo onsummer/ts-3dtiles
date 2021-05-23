9f64d7fdc20a50dd62ebb659dd89b9f824f4354e
onsummer <onsummer@foxmail.com>
2021-05-23 15:17:57 +0800

    [remove] some math functions.

--- src/core/matrix/mat3.ts
+++
@@ -1,7 +0,0 @@
-class Mat3 {
-  v11: number = 1; v12: number = 0; v13: number = 0
-  v21: number = 0; v22: number = 1; v23: number = 0
-  v31: number = 0; v32: number = 0; v33: number = 1
-}
-
-export default Mat3
--- src/core/vector/vec4.ts
+++
@@ -1,8 +0,0 @@
-class Vec4 {
-  v1 = 0
-  v2 = 0
-  v3 = 0
-  v4 = 0
-}
-
-export default Vec4
